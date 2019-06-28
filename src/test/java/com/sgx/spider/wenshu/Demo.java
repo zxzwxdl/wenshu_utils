f3f6334795897d0a276f9936b531f880ccf764ec
宋国雄 <489406796@qq.com>
2019-06-28 08:40:08 +0800

    update wzws

--- src/test/java/com/sgx/spider/wenshu/Demo.java
+++ src/test/java/com/sgx/spider/wenshu/Demo.java
@@ -9,7 +9,6 @@ import com.sgx.spider.wenshu.vl5x.Number;
 import com.sgx.spider.wenshu.vl5x.Vjkl5;
 import com.sgx.spider.wenshu.vl5x.Vl5x;
 import com.sgx.spider.wenshu.wzws.WZWSParser;
-import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
@@ -18,6 +17,7 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -26,40 +26,67 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 
-import javax.script.ScriptException;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class Demo {
-    public static final String DOMAIN = "wenshu.court.gov.cn";
+    private static final String DOMAIN = "wenshu.court.gov.cn";
 
-    public static RequestConfig requestConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build();
-
-    public static BasicCookieStore cookieStore = new BasicCookieStore();
-
-    public static CloseableHttpClient httpClient = HttpClients.custom()
+    private static RequestConfig requestConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build();
+    private static BasicCookieStore cookieStore = new BasicCookieStore();
+    private static CloseableHttpClient httpClient = HttpClients.custom()
             .setDefaultRequestConfig(requestConfig)
             .setDefaultCookieStore(cookieStore)
             .build();
 
     @Test
-    public void requestList() throws Exception {
+    public void listPage() throws Exception {
+        String initUrl = "http://wenshu.court.gov.cn/list/list";
+        HttpGet httpGet = new HttpGet(initUrl);
+
+        String text;
+        try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+            text = EntityUtils.toString(response.getEntity(), "UTF-8");
+        }
+
+        int retry = 3;
+        boolean success = false;
+        for (int i = 0; i < retry; i++) {
+            if (text.contains("请开启JavaScript并刷新该页")) {
+                String redirectUrl = WZWSParser.parse(text);
+                System.out.println(redirectUrl);
+                try (CloseableHttpResponse response = httpClient.execute(new HttpGet(redirectUrl))) {
+                    text = EntityUtils.toString(response.getEntity(), "UTF-8");
+                }
+            } else {
+                success = true;
+                break;
+            }
+        }
+        if (!success) {
+            throw new Exception("fail");
+        }
+
         String url = "http://wenshu.court.gov.cn/List/ListContent";
         HttpPost httpPost = new HttpPost(url);
 
-        httpPost.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36");
-        httpPost.addHeader("X-Requested-With", "XMLHttpRequest");
-
-        Vjkl5 vjkl5 = new Vjkl5();
+        Vjkl5 vjkl5 = null;
+        for (Cookie c : cookieStore.getCookies()) {
+            if (Objects.equals(c.getName(), "vjkl5")) {
+                vjkl5 = new Vjkl5(c.getValue());
+                break;
+            }
+        }
+        if (vjkl5 == null) {
+            vjkl5 = new Vjkl5();
+            BasicClientCookie cookie = new BasicClientCookie("vjkl5", vjkl5.getValue());
+            cookie.setDomain(DOMAIN);
+            cookieStore.addCookie(cookie);
+        }
 
         List<NameValuePair> data = new ArrayList<>();
-        // TODO 太多人测试此检索条件的数据，如果报错或返回脏数据请更换别的检索条件进行测试
         data.add(new BasicNameValuePair("Param", "案件类型:刑事案件"));
-
         data.add(new BasicNameValuePair("Index", "1"));
         data.add(new BasicNameValuePair("Page", "10"));
         data.add(new BasicNameValuePair("Order", "法院层级"));
@@ -69,14 +96,8 @@ public class Demo {
         data.add(new BasicNameValuePair("guid", new Guid().getValue()));
         httpPost.setEntity(new UrlEncodedFormEntity(data, "UTF-8"));
 
-        BasicClientCookie cookie = new BasicClientCookie("vjkl5", vjkl5.getValue());
-        cookie.setDomain(DOMAIN);
-        cookieStore.addCookie(cookie);
-
-        String text;
         try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
-            HttpEntity entity = response.getEntity();
-            text = EntityUtils.toString(entity);
+            text = EntityUtils.toString(response.getEntity());
         }
 
         List<JSONObject> jsonObjects = JSON.parseArray((String) JSON.parse(text), JSONObject.class);
@@ -100,34 +121,35 @@ public class Demo {
     }
 
     @Test
-    public void requestDetail() throws URISyntaxException, IOException, ScriptException, NoSuchMethodException {
+    public void detailPage() throws Exception {
         String url = "http://wenshu.court.gov.cn/CreateContentJS/CreateContentJS.aspx";
 
         URIBuilder uriBuilder = new URIBuilder(url);
-        // TODO 太多人测试此DocID，如果报错请更换DocID进行测试
         uriBuilder.addParameter("DocID", "8f0230e9-f0e4-418f-a8e0-4c24677c7a79");
 
-        URI builtUrl = uriBuilder.build();
-        HttpGet httpGet = new HttpGet(builtUrl);
+        HttpGet httpGet = new HttpGet(uriBuilder.build());
 
         String text;
         try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
-            HttpEntity entity = response.getEntity();
-            text = EntityUtils.toString(entity, "UTF-8");
+            text = EntityUtils.toString(response.getEntity(), "UTF-8");
         }
 
-        if (text.contains("请开启JavaScript并刷新该页")) {
-            // 这样更快
-            String redirectUrl1 = WZWSParser.parse(text, builtUrl);
-            // 这样慢很多
-            String redirectUrl2 = WZWSParser.parse(text);
+        int retry = 3;
+        boolean success = false;
+        for (int i = 0; i < retry; i++) {
+            if (text.contains("请开启JavaScript并刷新该页")) {
+                String redirectUrl = WZWSParser.parse(text);
 
-            assert Objects.equals(redirectUrl1, redirectUrl2);
-
-            try (CloseableHttpResponse response = httpClient.execute(new HttpGet(redirectUrl1))) {
-                HttpEntity entity2 = response.getEntity();
-                text = EntityUtils.toString(entity2, "UTF-8");
+                try (CloseableHttpResponse response = httpClient.execute(new HttpGet(redirectUrl))) {
+                    text = EntityUtils.toString(response.getEntity(), "UTF-8");
+                }
+            } else {
+                success = true;
+                break;
             }
+        }
+        if (!success) {
+            throw new Exception("fail");
         }
 
         System.out.println(text);
